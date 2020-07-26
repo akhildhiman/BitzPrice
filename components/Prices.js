1df67e3d857fe3c7c664ef3db81a054e5594ed67
akhildhiman <akhilblackmetal69@gmail.com>
2020-07-26 23:35:30 +0530

    replace Prices

--- components/Prices.js
+++ components/Prices.js
@@ -1,34 +1,30 @@
-class Prices extends React.Component {
-  state = {
-    currency: "USD",
-  }
+import { useState } from "react"
 
-  render() {
-    console.log(this.props)
-    const { currency } = this.state
-    const { bpi } = this.props.bpiData
-    return currency ? (
-      <div>
-        <ul className="list-group">
-          <li className="list-group-item">
-            Bitcoin rate for {bpi[this.state.currency].description} :
-            <span className="badge badge-primary">{bpi[currency].code}</span>
-            <strong>{bpi[currency].rate}</strong>
-          </li>
-        </ul>
+const Prices = ({ bpiData }) => {
+  const [currency, setCurrency] = useState("USD")
+  const bpi = bpiData.bpi
 
-        <select
-          className="form-control"
-          onChange={(e) => this.setState({ currency: e.target.value })}
-        >
-          <option value="USD">USD</option>
-          <option value="GBP">GBP</option>
-          <option value="EUR">EUR</option>
-        </select>
-      </div>
-    ) : null
-  }
-  h
+  return currency ? (
+    <div>
+      <ul className="list-group">
+        <li className="list-group-item">
+          Bitcoin rate for {bpi[currency].description} :
+          <span className="badge badge-primary">{bpi[currency].code}</span>
+          <strong>{bpi[currency].rate}</strong>
+        </li>
+      </ul>
+
+      <select
+        onChange={(e) => setCurrency(e.target.value)}
+        value={currency}
+        className="form-control"
+      >
+        <option value="USD">USD</option>
+        <option value="GBP">GBP</option>
+        <option value="EUR">EUR</option>
+      </select>
+    </div>
+  ) : null
 }
 
 export default Prices
